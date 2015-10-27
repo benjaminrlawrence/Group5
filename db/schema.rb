7859c236d8302f7e17974b79c39dae7c1186723d
Ben Lawrence <benjaminrlawrence@gmail.com>
2015-10-26 22:06:58 -0400

    Edited Main Page to Include Search Checkboxes and created model for queries with relationship defined to User Table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020013455) do
+ActiveRecord::Schema.define(version: 20151027020143) do
+
+  create_table "queries", force: :cascade do |t|
+    t.string   "search_string"
+    t.string   "type"
+    t.string   "email"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
